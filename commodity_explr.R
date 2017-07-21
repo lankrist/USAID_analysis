3d658b86a7ff3ce1537e68f8e16db3384a76a632
lankrist <lankrist@umich.edu>
2017-07-21 13:42:47 -0400

    dataset updates

--- commodity_explr.R
+++ commodity_explr.R
@@ -1,7 +1,7 @@
 #2015-2017 commodity data 
 library(ggplot2)
 library(scales) #pretty_breaks()
-commodity <- read.csv("~/USAID_Internship2017/dataset/country_commodity_20170718.csv", stringsAsFactors=FALSE)
+commodity <- read.csv("~/USAID_Internship2017/dataset/country_commodity_TO1_SO.csv", stringsAsFactors=FALSE)
 
 names(commodity)
 # "Country"                "Task.Order"             "Information.Provider"  
@@ -24,14 +24,15 @@ commodity[grepl("Condom", commodity$Global.Product),"commodity.category"] = "Con
 commodity[grepl("Tablet", commodity$Global.Product), "commodity.category"] = "Drug" 
 commodity[grepl("mg/mL", commodity$Global.Product), "commodity.category"] = "Drug" 
 commodity[grepl("Pellets", commodity$Global.Product), "commodity.category"] = "Drug" 
-commodity[grepl("Kit", commodity$Global.Product), "Global.Product"]
+commodity[grepl("HIV", commodity$Global.Product), "Global.Product"]
 commodity[grepl("MC Kit", commodity$Global.Product), "commodity.category"] = "VMMC"
 
-commodity[grepl("COBAS", commodity$Global.Product), "commodity.category"] = "COBAS"
+commodity[grepl("Pallet", commodity$Global.Product), "commodity.category"] = "Pallet"
+commodity[grepl("COBAS", commodity$Global.Product), "Global.Product"] = "COBAS"
 commodity[grepl("mSystems", commodity$Global.Product), "commodity.category"] = "mSystems"
 
 
-commodity[commodity$commodity.category == "NA", "Global.Product"]
+commodity[commodity$commodity.category == "Drug", "Global.Product"]
 
 #BLANK THEME
 blank_theme = theme_minimal() + 
@@ -44,13 +45,16 @@ blank_theme = theme_minimal() +
     plot.title = element_text(size = 14, face = "bold")
   )
 
-dev.new(width = 5, height = 4)
+dev.new(width = 8, height = 8)
 
-#Order data
+#Order data (not necessary)
 commodity = commodity[order(commodity$Product.Line),]
 
+total = sum(commodity$PO.Line.Item.Cost)
+total2017 = sum(commodity[commodity$PO.Received.Year == 2017,"PO.Line.Item.Cost"])
+
 #Pie chart that shows how much commodities cost by profuct line
-bp = ggplot(commodity, aes(x = "", y = PO.Line.Item.Cost, fill = Product.Line))+
+bp = ggplot(commodity, aes(x = "", y = PO.Line.Item.Cost, fill = commodity.category))+
   geom_bar(width = 1, stat = "identity")
 bp
 
@@ -60,18 +64,21 @@ pie + scale_fill_brewer("Commodity Expense") + blank_theme +
   theme(axis.text.x =element_blank())+
   labs(title = '2015-2017 Commodity Expense by Product Line 20170718 BI&A')
 
+dev.new()
 #Box plot that shows unit cost of commodities by direct drop vs warehouse
 threshold = 100
 com2 = commodity[commodity$PO.Line.Item.Unit.Cost <= threshold,]
 
-sp = ggplot(com2, aes(x = Product.Line, y = PO.Line.Item.Unit.Cost, 
+sp = ggplot(com2, aes(x = commodity.category, y = PO.Line.Item.Unit.Cost, 
                       color = Supply.Chain.Framework, shape = Supply.Chain.Framework))+
   geom_boxplot()
 
 sp + blank_theme + labs(title = paste("2015-2017 Commodity Unit Cost by Product Line (under $", threshold, ")"))
 #show count on products
+commodity[grepl("NA", commodity$commodity.category), "Global.Product"]
 
 #Stacked area chart
+dev.new()
 comsac = aggregate(PO.Line.Item.Cost~PO.Received.Year+Country, commodity, sum)
 comsa = expand.grid(unique(commodity$PO.Received.Year), levels(commodity$Country),0)
 colnames(comsa) = names(comsac)
@@ -86,5 +93,6 @@ sac + geom_area()+
                    title = "Commodity Cost by Country from 2015-2017")+
   scale_x_continuous(breaks = unique(coms$PO.Received.Year)) #displays only integers
 
-
-
+countryTotal= aggregate(PO.Line.Item.Cost~Country, com, sum)
+ct = head(countryTotal[order(-countryTotal$PO.Line.Item.Cost),])
+#join with commodity data frame
