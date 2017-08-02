3b16d600b294f0982abe8c684d9294454642dfac
lankrist <lankrist@umich.edu>
2017-08-02 16:49:27 -0400

    zambia data cleaning/ no lab/ show tracker information missing in ARTMIS

--- data_cleaning/zambia_data_clean.R
+++ data_cleaning/zambia_data_clean.R
@@ -1,25 +1,38 @@
 #zambia data
 library(data.table)
+library(plyr)
 setwd("/Documents/USAID_Internship2017")
 
 zam = read.csv(file = "dataset/orders/Current Zambia Orders - June 19 2017.csv", header = T, stringsAsFactors = F)
 names(zam)
-
 zamA = read.csv(file = "dataset/ARTMIS/ALL ORDERS_report_20170802.csv", header = T, stringsAsFactors = F)
 names(zamA)
-nrow(zamA)
-zamt = zam[,c("PSM.RO..","PO..","Item.Description", "Quantity", "Delivery.Date")]
+zamt = zam[,c("PSM.RO..","PO..", "ComUSAID.1", "Item.Description", "Quantity", "Delivery.Date")]
 zamt$tracker = "Tracker"
-zamAt= zamA[,c("RO.Number", "PO.DO.IO.Number", "Product.Name", "Ordered.Quantity", "Actual.Delivery.Date")]
+zamAt= zamA[,c("RO.Number", "PO.DO.IO.Number", "Commodity.SubCategory.Description",  "Product.Name", "Ordered.Quantity", "Actual.Delivery.Date")]
 zamAt$tracker = "ARTMIS"
-nrow(zamAt)
-
 colnames(zamt) = colnames(zamAt)
 
+#exclude replenishment orders, these are sent to warehouse not to country
+zamAt = zamAt[zamAt$RO.Number != "Replenishment Orders",]
+
 zamc = rbind(zamt, zamAt)
-
-zamc$Ordered.Quantity
-
 zamc$Ordered.Quantity=as.numeric(gsub(pattern=",", "", zamc$Ordered.Quantity))
 
-zamc[order(zamc$RO.Number, zamc$PO.DO.IO.Number, zamc$Product.Name, zamc$Ordered.Quantity), ]
+#EXCLUDED laboratory commodities
+zamc2 =zamc[!grepl("Laboratory", zamc$Commodity.SubCategory.Description), ]
+# 
+zamo = zamc2[order(zamc2$RO.Number, zamc2$PO.DO.IO.Number, zamc2$Product.Name), ] #ordered non_Lab RO
+# 
+# #as.Date(zam$Estimated.Delivery.Date, by = "%d-%b-%Y")
+
+zamdt= as.data.table(zamc2)[,sum(Ordered.Quantity, na.rm = T), by = .(RO.Number, PO.DO.IO.Number,tracker)]
+zamu = zamdt[duplicated(zamdt[,c("RO.Number", "PO.DO.IO.Number", "V1")]),]
+#12 matches
+zam2 = zamc2[!(zamc2$RO.Number %in% zamu$RO.Number),] #find tracker ROs not in ARTMIS
+zam3 = subset(zam2,zam2$tracker== "Tracker")[,"RO.Number"]
+zam3= unique(zam3[zam3 != ""]) #list of tracker ROs not in ARTMIS
+
+z = zamc2[zamc2$RO.Number %in% zam3, ]
+
+write.csv(x= z, file = "dataset/orders/zambia_ARTMIS_missing_RO.csv")
