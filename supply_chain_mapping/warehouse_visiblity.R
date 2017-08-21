65a3b69efeaf530e3c960f8ebd987c022186dab5
lankrist <lankrist@umich.edu>
2017-08-21 11:25:02 -0400

    use site level last push

--- supply_chain_mapping/warehouse_visiblity.R
+++ supply_chain_mapping/warehouse_visiblity.R
@@ -16,20 +16,26 @@ stock_country = c("Angola", "Cameroon", "Ethiopia", "Ghana", "Guyana", "Haiti",
                   "Rwanda", "Swaziland", "United Republic of Tanzania", 
                   "Uganda", "Vietnam", "Zambia", "Zimbabwe")
 
-stock_country %in% countries$ADMIN 
-
+#stock_country %in% countries$ADMIN 
 countries$national_stock = NA
 countries$site_stock = NA
 countries$Distribution_point_stock = NA
 
-nat = c(rep(1, 4), 0, rep(1, 2), 0, rep(1, 6), rep(0,2), rep(1, 2))
-site = c(1, 0, 1, 0, 0, rep(1, 4), 0, 1, rep(0, 5), rep(1, 2))
-distp = c(0, rep(1,16),0)
+nat = as.logical(c(rep(1, 4), 0, rep(1, 2), 0, rep(1, 6), rep(0,2), rep(1, 2)))
+site = as.logical(c(1, 0, 1, 0, 0, rep(1, 4), 0, 1, rep(0, 5), rep(1, 2)))
+distp = as.logical(c(0, rep(1,16),0))
 visibility = as.data.frame(cbind(stock_country, nat, site, distp))
-visibility[,2:4] = sapply(visibility[,2:4], as.numeric)-1
+#visibility[,2:4] = sapply(visibility[,2:4], as.numeric)-1
+visibility[,2:4] = sapply(visibility[,2:4],as.logical)
 
 stock_ = merge(countries, visibility, by.x= "ADMIN", by.y = "stock_country")
-stock_$visibility = stock_$nat+stock_$site*2+stock_$distp*3
+
+
+YN = function(logical){
+  return(ifelse(logical ==T, "Yes", "No"))
+}
+YN(NA)
+
 
 pal <- colorNumeric(
   palette = "Blues",
@@ -40,17 +46,20 @@ m = leaflet(stock_) %>% addTiles() %>%
     icon="fa-globe", title="Zoom to World View",
     onClick=JS("function(btn, map){ map.setZoom(2); }"))) %>%
   addPolygons(stroke = FALSE, smoothFactor = 0.2, 
-            fillOpacity = 1, color = ~pal(stock_$visibility), 
+            fillOpacity = 1, 
             label = stock_$ADMIN,
-            popup = paste("National Stock:" , stock_$nat, "<br>",
-                          "Site Stock:", stock_$site, "<br>",
-                          "Distribution Point Stock:", stock_$distp,"<br>"),
+            popup = paste("National Stock:" , YN(stock_$nat), "<br>",
+                          "Site Stock:", YN(stock_$site), "<br>",
+                          "Distribution Point Stock:", YN(stock_$distp),"<br>"),
             highlight = highlightOptions(
               weight = 5,
               color = "#888",
               fillOpacity = 0.7,
               bringToFront = TRUE),
-            group = "Orders") %>%
+            group = "Orders") 
+
+
+#%>%
   addLegend("bottomright", pal = pal, values = ~visibility,
             title = "Visibility",
             opacity = 1)
